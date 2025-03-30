b55b5d3f4185d49c02d98421e5f195df9428bb39
Kuroam <kuroam.dev@gmail.com>
2025-03-30 14:54:03 +0330

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # md2html-compiler
 A simple and efficient Markdown-to-HTML compiler that converts .md files into clean, structured HTML.
+
+# 📥 Installation
+
+just install packages with
+```bash
+npm i
+```
+
+then run with:
+```bash
+node index.js
+```
+
+# 🚀 Usage
+just create a markdown file into `Markdown` directory and write what you want to compile into html and run:
+```bash 
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+const { marked } = require('marked')
+
+const markdownDirectoryPath = path.join(__dirname, 'Markdown')
+const outputDirectoryPath = path.join(__dirname, 'Html')
+
+fs.readdirSync(markdownDirectoryPath).forEach((markdown) => {
+    const markedDown = fs.readFileSync(markdownDirectoryPath + '/' + markdown, 'utf-8');
+    const markdownParts = markdown.split(/\./)
+    fs.appendFileSync(outputDirectoryPath + '/' + markdownParts[0] + '.html', `<!DOCTYPE html>
+            <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Markdown Viewer</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        margin: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="content">
+      ${marked(markedDown)}
+    </div>
+  </body>
+</html>`, (err) => {
+        throw new Error('there was an error while creating compiled file!\n', err)
+    })
+
+})
+
+
+
+
+
